03d95900fa2ddd7dfe2e295e27f33233ba4a047d
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-06-11 12:18:36 +0200

    refacto

--- syncRepositories.js
+++ syncRepositories.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const db = require('./db');
 const axios = require('axios');
-const { repository } = require('./db');
 
 function getUserRepos(username) {
   return axios
@@ -15,36 +14,39 @@ function getUserRepos(username) {
 
 async function run() {
   const users = await db.user.findMany();
-
   const syncUserPromises = users.map(async (user) => {
     console.log(`getting repos for ${user.githubUsername}`);
-    const repositories = await getUserRepos(user.githubUsername);
-    console.log(`got repos for ${user.githubUsername}`);
-
-    const syncReposPromises = repositories
-      .map((repo) => ({
-        url: repo.html_url,
-        userId: user.id,
-      }))
-      .map((repo) => {
-        return db.repository.upsert({
-          create: repo,
-          update: repo,
-          where: { url: repo.url },
-        });
+    let repositories = [];
+    try {
+      repositories = await getUserRepos(user.githubUsername);
+      console.log(`got repos for ${user.githubUsername}`);
+      const syncReposPromises = repositories
+        .map((repo) => ({
+          url: repo.html_url,
+          userId: user.id,
+        }))
+        .map((repo) =>
+          db.repository.upsert({
+            create: repo,
+            update: repo,
+            where: { url: repo.url },
+          })
+        );
+      await Promise.all(syncReposPromises);
+      await db.user.update({
+        where: { id: user.id },
+        data: { reposLastSyncDate: new Date() },
       });
-
-    await Promise.all(syncReposPromises);
-    await db.user.update({
-      where: { id: user.id },
-      data: { reposLastSyncDate: new Date() },
-    });
-    console.log(`all repos for ${user.githubUsername} synced to db`);
+      console.log(`all repos for ${user.githubUsername} synced to db`);
+    } catch (err) {
+      console.log(`cannot sync repos for ${user.githubUsername}`);
+      console.error(err);
+    }
   });
-  await Promise.all(syncUserPromises);
-  console.log('done');
+  return Promise.all(syncUserPromises);
 }
 
 run().then(() => {
+  console.log('done');
   db.$disconnect();
 });
